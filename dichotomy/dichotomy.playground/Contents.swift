647281f5d592f96fbaa8a474faf747493aa89e1d
sylenthwave <sylenthwave@gmail.com>
2020-01-30 17:21:55 +0800

    dichotomy normal implementation

---
+++ dichotomy/dichotomy.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var array = Array(0...100000)
+
+func dichotomyFindIndex(array: [Int], target: Int) -> Int {
+    var startIndex = 0
+    var endIndex = array.count - 1
+    var times = 0
+    
+    var targetIndex = (startIndex + endIndex) / 2
+    guard targetIndex < (array.count - 1) else {
+        return 0
+    }
+    
+    while array[targetIndex] != target {
+        // calculated times
+        times += 1
+        
+        if array[targetIndex] > target {
+            //left
+            endIndex = targetIndex - 1
+        } else {
+            //right
+            startIndex = targetIndex + 1
+        }
+        targetIndex = startIndex + (endIndex - startIndex) / 2
+        print("target index is \(targetIndex)")
+    }
+    print("find target = \(array[targetIndex]), at index = \(targetIndex), times = \(times)")
+    return targetIndex
+}
+
+dichotomyFindIndex(array: array, target: 500)
